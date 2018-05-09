042c4ca8780f1f57b09e6410a62709d8ae30e3dc
Henry Townley <htownley@me.com>
2018-05-09 05:19:29 -0400

    added url and file functionality to advanced_policy_parser.py

--- advanced_policy_parser.py
+++ advanced_policy_parser.py
@@ -13,17 +13,39 @@ Download:
 ''' 
 
 import os
-import textwrap
-from gensim.models import KeyedVectors
 import time
 import sys
 
+import textwrap
+from gensim.models import KeyedVectors
+import requests
 
+from policy_grabber import policy_grabber
+
+
+###############################################################
+# Load pretrained Google word2vec model
+def load_word2vec_model():
+	print("Loading pretrained Google word2vec model...")
+	print("expected time on the order of 60 sec")
+	start_time = time.time()
+	filename = 'GoogleNews-vectors-negative300.bin'
+	model = KeyedVectors.load_word2vec_format(filename, binary=True)
+	testing123 = model.most_similar(positive=["testing"], topn=20)
+	print("Successfully loaded model ({} sec)".format(time.time() - start_time))
+	return model
+
+model = load_word2vec_model()
+
+## for testing purposes:
+# while True:
+# 	test_keyword = input("keyword:")
+# 	for word in keyword_expander(test_keyword, 10, model):
+
+
+###############################################################
 # start main function
-def main():
-
-	# Load pretrained Google word2vec model
-	model = load_word2vec_model()
+def main(model):
 
 	###############################################################
 	# Introduction:
@@ -32,23 +54,72 @@ def main():
 			"=======================================================\n"
 			"Have you got a dense, opaque, unreadable pile of legal\n"
 			"privacy jargon that you can't make heads or tails of?\n"
-			"I can help with that! Just let me know where to look:\n"
-			"(<filename.txt> OR <path-to-filename.txt>)"))
+			"I can help with that! Just let me know where to look.\n"))
+
+	##############################################################
+	# figure out where the privacy policy text is
+	url_bool = False
+	file_bool = False
+
+	while True:
+		print("Please type either \"url\" or \"file\" to indicate whether\n"
+			"the privacy policy can be found online or in a local text file:")
+		print("type \"quit\" to exit.")
+
+		user_input = input()
+
+		if user_input == "quit":
+			exit()
+		elif user_input == "url":
+			url_bool = True
+			break
+		elif user_input == "file":
+			file_bool = True
+			break
 
 
 	##############################################################
-	# get path to privacy policy text file
-	while(True):
+	# load privacy policy into paragraphs list
+	all_paragraphs = []
 
-		path_to_policy = input()
-		print()
+	if url_bool:
+		print("\nPlease enter the full URL:")
+		print("(<URL> OR \"done\")")
 
-		# check to make sure filename is valid
-		if not os.path.isfile(path_to_policy):
-			print("Invalid file or path. Please try again:")
-			print("(<filename.txt> OR <path-to-filename.txt> OR \"quit\")")
-		else:
-			break
+		while(True):
+			user_input = input()
+			if user_input == "done":
+				return
+
+			# check to see if URL is valid
+			try:
+				request = requests.get(user_input)
+				if request.status_code == 200:
+					all_paragraphs = policy_grabber(user_input)
+					break
+			except Exception as err:
+				print()
+				print(err)
+				print("Please try again:")
+				print("(<URL> OR \"done\")")
+
+	if file_bool:
+		print("\nPlease enter the file or filename:")
+		print("(<filename.txt> OR <path-to-filename.txt> OR \"done\")")
+
+		while(True):
+			user_input = input()
+			if user_input == "done":
+				return
+
+			# check to see if filename is valid
+			if os.path.isfile(user_input):
+				with open(user_input, 'r') as file:
+					all_paragraphs = file_to_paragraph(file)
+				break
+			else:
+				print("\nInvalid filename. Please try again:")
+				print("(<filename.txt> OR <path-to-filename.txt> OR \"done\")")
 
 
 	##############################################################
@@ -66,16 +137,16 @@ def main():
 			# get keyword to search for
 			if first_time:
 				first_time = False
-				keyword = input("What keyword should I search for?  ")
+				keyword = input("\nWhat keyword should I search for?  ")
 			else:
 				keyword = input(
 					"\n--------------------------------------------------------\n"
 					"Would you like to try again? What keyword should I search\n"
-					"for this time? (or \"quit\")   ")
+					"for this time? (or \"done\")   ")
 
 			# quit if necessary
-			if keyword == "quit":
-				exit()
+			if keyword == "done":
+				return
 
 			#####################################
 			# Find list of words related to keyword to search for in text file
@@ -89,43 +160,44 @@ def main():
 		#####################################
 		# analyze text file
 		print()
-		print("Analyzing " + path_to_policy + "...")
+		print("Analyzing " + user_input + "...")
 		print()
 
-		all_sentences = []
-		relevant_sentences = []
+		relevant_text = []
 
-		with open(path_to_policy, 'r') as file:
-
-			all_sentences = file_to_sentence(file)
-
-			for sentence in all_sentences:
-				if text_is_relevant(sentence, all_keywords):
-					relevant_sentences.append(sentence)
+		for paragraph in all_paragraphs:
+			if text_is_relevant(paragraph, all_keywords):
+				relevant_text.append(paragraph)
 
 
 		#####################################
 		# print findings
-		if len(relevant_sentences) > 0:
-			print("I found {} sentences related to the keyword {}. Would you like to see them?".format(len(relevant_sentences), keyword))
+		if len(relevant_text) > 0:
+			print("I found {} paragraphs related to the keyword {}. Would you like to see them?".format(len(relevant_text), keyword))
 			response = input("(\"yes\" OR \"no\"):  ")
 
 			if response == "yes":
 				counter = 1
-				for sentence in relevant_sentences:
-					sentence = textwrap.wrap(sentence)
-					print(str(counter) + ": " + sentence[0])
-					if len(sentence) > 0:
-						for piece in sentence[1:]:
+				for text in relevant_text:
+					text = textwrap.wrap(text)
+					print(str(counter) + ": " + text[0])
+					if len(text) > 0:
+						for piece in text[1:]:
 							print("   " + piece)
 
 					counter += 1
 		else:
-			print("I found {} sentences with the keyword {}.".format(len(relevant_sentences), keyword))
+			print("I found {} paragraphs related to the keyword {}.".format(len(relevant_text), keyword))
 
 
 
 		input("Hit any key to continue.")
+
+##########  End of main()  ####################################
+
+
+###############################################################
+# Various auxiliary functions:
 
 
 # returns list of words similiar to keyword
@@ -137,6 +209,8 @@ def keyword_expander(keyword, topn, model):
 
 	result = result + result_lower + result_title
 	result = [tple[0].lower().replace("_", " ") for tple in result]
+	result.append(keyword)
+
 	print(len(result))
 	result = list(set(result))
 	print(len(result))
@@ -158,30 +232,27 @@ def file_to_sentence(file):
 
 	return all_sentences
 
+def file_to_paragraph(file):
+	all_paragraphs = []
 
-def load_word2vec_model():
-	###############################################################
-	# Load pretrained Google word2vec model
-	print("Loading pretrained Google word2vec model...")
-	print("expected time on the order of 60 sec")
-	start_time = time.time()
-	filename = 'GoogleNews-vectors-negative300.bin'
-	model = KeyedVectors.load_word2vec_format(filename, binary=True)
-	testing123 = model.most_similar(positive=["testing"], topn=20)
-	print("Successfully loaded model ({} sec)".format(time.time() - start_time))
-	return model
+	for line in file:
+		all_paragraphs.append(line)
+
+	return all_paragraphs
+
 
 def text_is_relevant(text, keywords):
 
 	for keyword in keywords:
-		if keyword in text:
+		if keyword in text.lower():
 			return True
 	return False
 
 
+while True:
 
-if __name__== "__main__":
-  main()
+	if __name__== "__main__":
+		main(model)
 
 
 
--- policy_grabber.py
+++ policy_grabber.py
@@ -4,17 +4,14 @@ import urllib3
 import certifi
 from bs4 import BeautifulSoup
 
+def policy_grabber(url):
+	http = urllib3.PoolManager(
+	    cert_reqs='CERT_REQUIRED',
+	    ca_certs=certifi.where())
+	r = http.request('GET', url)
+	soup = BeautifulSoup(r.data, "html5lib")
 
-url = input("Please paste your URL in quotes: ")
-url = url[1:-1]
-http = urllib3.PoolManager(
-    cert_reqs='CERT_REQUIRED',
-    ca_certs=certifi.where())
-r = http.request('GET', url)
-soup = BeautifulSoup(r.data, "html5lib")
+	paragraphs = soup.find_all('p')
+	paragraphs = [p.get_text() for p in paragraphs]
 
-paragraphs = soup.find_all('p')
-
-for p in paragraphs:
-    print(p.text)
-    print("\n")
+	return paragraphs
