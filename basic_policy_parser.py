176a78b387e88b4312452bed32420c04cf478451
Henry Townley <htownley@me.com>
2018-05-09 01:20:06 -0400

    analyzes text file by sentence instead of by line

--- basic_policy_parser.py
+++ basic_policy_parser.py
@@ -5,9 +5,10 @@ internet browsing. Analyzes those policies and helps you understand
 what they might be saying.
 ''' 
 
-import os 
+import os
+import textwrap
 
-
+###############################################################
 # Introduction:
 print(("\n=======================================================\n"
 		"Hello There! Welcome to The Privacy Policy Memeifier.\n"
@@ -18,7 +19,7 @@ print(("\n=======================================================\n"
 		"(<filename.txt> OR <path-to-filename.txt>)"))
 
 
-
+##############################################################
 # get path to privacy policy text file
 while(True):
 
@@ -33,48 +34,76 @@ while(True):
 		break
 
 
+##############################################################
+# main loop:
+
 keyword = ""
 first_time = True
 
 while(True):
 
+	#####################################
 	# get keyword to search for
 	if first_time:
 		first_time = False
 		keyword = input("What keyword should I search for?  ")
 	else:
 		keyword = input(
-			"--------------------------------------------------------\n"
+			"\n--------------------------------------------------------\n"
 			"Would you like to try again? What keyword should I search\n"
 			"for this time? (or \"quit\")   ")
-	print()
+	keyword = keyword.lower()
 
 	# quit if necessary
-	if keyword.lower() == "quit":
+	if keyword == "quit":
 		exit()
 
-
+	#####################################
 	# analyze text file
 	print()
 	print("Analyzing " + path_to_policy + "...")
 	print()
 
+	all_sentences = []
 	relevant_sentences = []
 
 	with open(path_to_policy, 'r') as file:
 
+		sentence = []
+
 		for line in file:
-			if keyword in line.lower():
-				relevant_sentences.append(line)
+			for ch in line:
+				sentence.append(ch)
+
+				if sentence[-1] == "." or sentence[-1] == "\n":
+					sentence = "".join(sentence)
+					all_sentences.append(sentence)
+					sentence = []
+
+		for sentence in all_sentences:
+			if keyword in sentence:
+				relevant_sentences.append(sentence)
 
 
+	#####################################
 	# print findings
-	print("This is what I found:")
-	counter = 1
+	if len(relevant_sentences) > 0:
+		print("I found {} sentences with the keyword {}. Would you like to see them?".format(len(relevant_sentences), keyword))
+		response = input("(\"yes\" OR \"no\"):  ")
 
-	for sentence in relevant_sentences:
-		print(counter, sentence)
-		counter += 1
+		if response == "yes":
+			counter = 1
+			for sentence in relevant_sentences:
+				sentence = textwrap.wrap(sentence)
+				print(str(counter) + ": " + sentence[0])
+				if len(sentence) > 0:
+					for piece in sentence[1:]:
+						print("   " + piece)
+
+				counter += 1
+	else:
+		print("I found {} sentences with the keyword {}.".format(len(relevant_sentences), keyword))
+
 
 
 	input("Hit any key to continue.")
